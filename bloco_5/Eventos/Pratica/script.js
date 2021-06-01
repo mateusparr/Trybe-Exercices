5fb01dde5dc70c878ca813d75cfa3fc2d888048a
Pedro H Garcia <PH@Pedros-Macbook-Air.local>
2021-06-01 11:28:48 -0300

    eventos exercice 1 e 2 done

--- bloco_5/Eventos/Pratica/script.js
+++ bloco_5/Eventos/Pratica/script.js
@@ -12,5 +12,49 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
+
+  // Exercicio 1
+
+let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   
-  // Escreva seu código abaixo.
+function createDaysOfTheMonth() {
+  let getDayList = document.querySelector('#days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+        let day = dezDaysList[index];
+        let dayItem = document.createElement('li');
+      
+      if (day === 24 || day === 31) {
+          dayItem.className = 'day holliday';
+          dayItem.innerHTML = day;
+          getDayList.appendChild(dayItem);
+      } else if (day === 4 || day === 11) {
+          dayItem.className = 'day fiday';
+          dayItem.innerHTML = day;
+          getDayList.appendChild(dayItem);
+      }  else if (day === 25) {
+          dayItem.className = 'day holiday friday';
+          dayItem.innerHTML = day;
+          getDayList.appendChild(dayItem);
+      } else {
+          dayItem.className = 'day';
+          dayItem.innerHTML = day;
+          getDayList.appendChild(dayItem);
+      }
+  };
+};
+createDaysOfTheMonth();
+
+// Exercicio 2
+
+function createButton(buttonName) {
+    let buttonsContainer = document.querySelector('.buttons-container');
+    let btn  = document.createElement('button');
+    let btnID = 'btn-holiday';
+
+    btn.innerHTML = buttonName;
+    btn.id = btnID;
+    buttonsContainer.appendChild(btn);
+}
+
+createButton('Feriados');
